2ec63eaaee095c7a68c1b2c97297fb3758939661
Rao Fahad <raofahad046@gmail.com>
2021-07-12 08:50:40 -0700

    successfully added a feature of taking todos from console

--- dist/index.js
+++ dist/index.js
@@ -10,15 +10,52 @@ let todos = [
     new todoItem_1.TodoItem(4, "Call Joe", true),
 ];
 let collection = new todoCollection_1.TodoCollection("Adam", todos);
+let showCompleted = true;
 function displayTodoList() {
     console.log(`${collection.userName}'s Todo List ` +
         `(${collection.getItemCounts().incomplete} items to do )`);
-    collection.getTodoItems(true).forEach((item) => item.printDetails());
+    collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
 }
 var Commands;
 (function (Commands) {
+    Commands["Add"] = "Add New Task";
+    Commands["Complete"] = "Complete Task";
+    Commands["Toggle"] = "Show/Hide Completed";
+    Commands["Purge"] = "Remove Completed Task";
     Commands["Quit"] = "Quit";
 })(Commands || (Commands = {}));
+function promptAdd() {
+    console.clear();
+    inquirer
+        .prompt({ type: "input", name: "add", message: "Enter Task:" })
+        .then((answers) => {
+        if (answers["add"] !== "") {
+            collection.addTodo(answers["add"]);
+        }
+        promptUser();
+    });
+}
+function promptComplete() {
+    console.clear();
+    inquirer
+        .prompt({
+        type: "checkbox",
+        name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map((item) => ({
+            name: item.task,
+            value: item.id,
+            checked: item.complete,
+        })),
+    })
+        .then((answers) => {
+        let completedTasks = answers["complete"];
+        collection
+            .getTodoItems(true)
+            .forEach((item) => collection.markComplete(item.id, completedTasks.find((id) => id === item.id) != undefined));
+        promptUser();
+    });
+}
 function promptUser() {
     console.clear();
     displayTodoList();
@@ -30,8 +67,25 @@ function promptUser() {
         choices: Object.values(Commands),
     })
         .then((answers) => {
-        if (answers["command"] !== Commands.Quit) {
-            promptUser();
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                }
+                else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                break;
         }
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,61 @@ let todos: TodoItem[] = [
 ];
 
 let collection: TodoCollection = new TodoCollection("Adam", todos);
+let showCompleted = true;
 
 function displayTodoList(): void {
 	console.log(
 		`${collection.userName}'s Todo List ` +
 			`(${collection.getItemCounts().incomplete} items to do )`
 	);
-	collection.getTodoItems(true).forEach((item) => item.printDetails());
+	collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
 }
 
 enum Commands {
+	Add = "Add New Task",
+	Complete = "Complete Task",
+	Toggle = "Show/Hide Completed",
+	Purge = "Remove Completed Task",
 	Quit = "Quit",
+}
+
+function promptAdd(): void {
+	console.clear();
+	inquirer
+		.prompt({ type: "input", name: "add", message: "Enter Task:" })
+		.then((answers) => {
+			if (answers["add"] !== "") {
+				collection.addTodo(answers["add"]);
+			}
+			promptUser();
+		});
+}
+
+function promptComplete(): void {
+	console.clear();
+	inquirer
+		.prompt({
+			type: "checkbox",
+			name: "complete",
+			message: "Mark Tasks Complete",
+			choices: collection.getTodoItems(showCompleted).map((item) => ({
+				name: item.task,
+				value: item.id,
+				checked: item.complete,
+			})),
+		})
+		.then((answers) => {
+			let completedTasks = answers["complete"] as number[];
+			collection
+				.getTodoItems(true)
+				.forEach((item) =>
+					collection.markComplete(
+						item.id,
+						completedTasks.find((id) => id === item.id) != undefined
+					)
+				);
+			promptUser();
+		});
 }
 
 function promptUser(): void {
@@ -34,8 +78,27 @@ function promptUser(): void {
 			choices: Object.values(Commands),
 		})
 		.then((answers) => {
-			if (answers["command"] !== Commands.Quit) {
-				promptUser();
+			switch (answers["command"]) {
+				case Commands.Toggle:
+					showCompleted = !showCompleted;
+					promptUser();
+					break;
+
+				case Commands.Add:
+					promptAdd();
+					break;
+
+				case Commands.Complete:
+					if (collection.getItemCounts().incomplete > 0) {
+						promptComplete();
+					} else {
+						promptUser();
+					}
+					break;
+
+				case Commands.Purge:
+					collection.removeComplete();
+					break;
 			}
 		});
 }
